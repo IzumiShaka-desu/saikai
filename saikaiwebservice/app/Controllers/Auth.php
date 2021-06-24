cd80081692225fa683ef76bcb477bcf0e56489ea
IzumiShaka-desu <shakaaji29@gmail.com>
2021-06-24 11:40:03 +0700

    bersih2

--- saikaiwebservice/app/Controllers/Auth.php
+++ saikaiwebservice/app/Controllers/Auth.php
@@ -1,61 +1,47 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
 
-class Auth extends ResourceController{
-    protected $format='json';
-        protected $modelName='App\Models\Auth_model';
-        function index(){
-            return $this->respond('',404);
-        }
-        public function login(){
-            $email=$this->request->getPost('email');
-            $password=$this->request->getPost('password');
-            $validation =  \Config\Services::validation();
-            $data=[
-                'email'=>$email,
-                'password'=>$password
-            ];
-            if($validation->run($data,'auth')==FALSE){
-                $response = [
-                    'status' => 500,
-                    'error' => true,
-                    'data' => $validation->getErrors(),
-                ];
-            }
-             else{   
-                 $result=$this->model->login($email,$password);
-                $response = [
-                    'status' => 200,
-                    'error' => false,
-                    'data' => $result,
-                ];}
-                return $this->respond($response, 200);
-        }
-        public function register(){
-            $email=$this->request->getPost('email');
-            $password=$this->request->getPost('password');
-            $fullname=$this->request->getPost('fullname');
-            $validation =  \Config\Services::validation();
-            $data=[
-                'email'=>$email,
-                'password'=>$password
-            ];
-            if($validation->run($data,'auth')==FALSE){
-                $response = [
-                    'status' => 500,
-                    'error' => true,
-                    'data' => $validation->getErrors(),
-                ];
-            }
-             else{   $result=$this->model->register($email,$password);
-                $response = [
-                    'status' => 200,
-                    'error' => false,
-                    'data' => $result,
-                ];}
-                return $this->respond($response, 200);
-        }
-}
+class Auth extends ResourceController
+{
+    protected $format = 'json';
+    protected $modelName = 'App\Models\Auth_model';
 
-?>
+    public function login()
+    {
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $data = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $result = $this->model->login($email, $password);
+        $response = [
+            'status' => 200,
+            'error' => false,
+            'data' => $result,
+        ];
+        return $this->respond($response, 200);
+    }
+    public function register()
+    {
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+        $fullname = $this->request->getPost('fullname');
+        $data = [
+            'email' => $email,
+            'password' => $password
+        ];
+        $result = $this->model->register($email, $password);
+        $response = [
+            'status' => 200,
+            'error' => false,
+            'data' => $result,
+        ];
+        return $this->respond($response, 200);
+    }
+}
